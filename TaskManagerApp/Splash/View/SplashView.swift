a31ae140dc0875ebe768353029541ad1f57a15f7
Matheus Evangelista <matheus.evangelista@g.globo>
2021-07-20 19:30:52 -0300

    Add App Icon

--- TaskManagerApp/Splash/View/SplashView.swift
+++ TaskManagerApp/Splash/View/SplashView.swift
@@ -14,7 +14,15 @@ struct SplashView: View {
     var body: some View {
         switch state {
         case .loading:
-            Text("Loading")
+            ZStack {
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .padding(20)
+                    .background(Color.white)
+                    .ignoresSafeArea()
+            }
         case .goToSignInScreen:
             Text("Carregar tela de login")
         case .goToHomeScreen:
