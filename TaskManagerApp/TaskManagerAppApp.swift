26029a3e2cddc989afc79d53befbcfe0a5afd374
Matheus Evangelista <matheus.evangelista@g.globo>
2021-07-20 18:38:44 -0300

    Initial Commit

---
+++ TaskManagerApp/TaskManagerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerAppApp.swift
+//  TaskManagerApp
+//
+//  Created by matheus.evangelista on 20/07/21.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
